48bb478e87f0c273a7efcbf1f346ca5078d87729
alexisteh <wenhui.teh23@gmail.com>
2020-07-07 10:08:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+
+let dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function(e) {
+    console.log(e.key);
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key == "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+    if (left > 0 && left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+
+
+
