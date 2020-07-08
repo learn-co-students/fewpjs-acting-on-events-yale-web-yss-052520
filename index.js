d132a39bf40dd33d9ca3816504f3fddf4fbc63f3
mlkelly <maurak87@gmail.com>
2020-07-08 08:35:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+// code given from lab readme
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+//   added code to make the dogger also move right
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+    }
+}
+
+// code given from lab readme
+document.addEventListener("keydown", function(e) {
+if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+}
+
+if (e.key === "ArrowRight") {
+    moveDodgerRight();
+}
+
+});
