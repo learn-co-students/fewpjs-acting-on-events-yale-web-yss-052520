39fb2bc3119f5c646c4821216641807938c65c61
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-20 19:56:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+const gamebox = document.getElementById('game')
+const gameboxRight = parseInt(gamebox.offsetWidth, 10)
+const dodgerWidth = parseInt(dodger.offsetWidth, 10)
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left, 10);
+
+  if (left + dodgerWidth < gameboxRight) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+      moveDodgerLeft(); break
+    case "ArrowRight":
+      moveDodgerRight(); break
+  }
+})
